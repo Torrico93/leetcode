0599cc107f0d5cbf3839dccee8d5d645338646a6
Sebastian <seba.torrico5@gmail.com>
2020-03-19 16:15:25 -0300

    add package with exercise one finished and add folder to gitignore

---
+++ src/com/leetcode1/exercise1/Solution.java
@@ -0,0 +1,27 @@
+package com.leetcode1.exercise1;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextByte();
+        int contador = 0;
+        for (int i = num; i >= 0; i--) {
+
+            if (num % 2 == 0 && num>0) {
+                num = num /2;
+                contador++;
+            } else if (num>0){
+                num--;
+                contador++;
+            }
+        }
+        System.out.println(contador);
+
+    }
+}
+
